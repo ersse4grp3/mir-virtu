ed6805a04dfc4182479f7ade47ef244082fc871c
ersse4grp3 <85520848+ersse4grp3@users.noreply.github.com>
2021-06-11 13:40:17 +0200

    fichier de configuration de Nextcloud

---
+++ Nextcloud-config.php
@@ -0,0 +1,22 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocejl5k371p8',
+  'passwordsalt' => 'Bi16YdGFYEarkrnJglWbIS+fvWGRos',
+  'secret' => 'sX041+FaJBfdmSfgCurr3KA23ZEoyG411hU9+LgRLmJrvYCJ',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.10.25',
+  ),
+  'datadirectory' => '/var/www/html/nextcloud/data',
+  'dbtype' => 'mysql',
+  'version' => '21.0.1.1',
+  'overwrite.cli.url' => 'http://192.168.10.25/nextcloud',
+  'dbname' => 'nextclouddb',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextclouduser',
+  'dbpassword' => 'next123',
+  'installed' => true,
+);
